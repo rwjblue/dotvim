d200433646b53a7de565bf9693217663f94e3c1b
Robert Jackson <rjackson@linkedin.com>
2022-08-19 14:15:11 -0400

    Refactor how `rwjblue.plugins` works
    1. always load `packer` in the root
    2. refactor `M.update` and `M.bootstrap` to call `packer.sync` directly
    3. update `M.update` to _actually_ update and write a new snapshot file
    4. fix an issue with treesitter plugin updates (have to actually
       **invoke** the function returned by `nvim-treesitter.install.update()`
    5. ensure `M.bootstrap` just calls `packer.install()` and
       `packer.compile()` (specifically **do not** call `packer.sync()`
       because we don't want to load new versions)

--- lua/rwjblue/plugins.lua
+++ lua/rwjblue/plugins.lua
@@ -17,21 +17,104 @@ local function check_or_install_packer()
   end
 end
 
-local function M.update(opts)
+check_or_install_packer()
+
+local packer = require 'packer'
+local util = require 'packer.util'
+local snapshot_path = util.join_paths(vim.fn.stdpath('config'), 'plugins-dev.json')
+
+packer.startup({
+  function(use)
+    use 'wbthomason/packer.nvim'
+    use 'neovim/nvim-lspconfig'
+    use 'tpope/vim-sensible'
+    use 'editorconfig/editorconfig-vim'
+    use 'tpope/vim-fugitive'
+    use 'tpope/vim-rhubarb' -- make fugitive understand github.com &co
+    use 'tpope/vim-git'
+    use 'tpope/vim-surround'
+    use 'christoomey/vim-tmux-navigator'
+    use 'airblade/vim-gitgutter'
+    use 'wincent/terminus'
+    use 'joshdick/onedark.vim'
+    use 'kyazdani42/nvim-web-devicons'
+
+    use {
+      'kyazdani42/nvim-tree.lua',
+      config = function()
+        require'nvim-tree'.setup { }
+      end
+    }
+
+    use {
+      "folke/trouble.nvim",
+      requires = "kyazdani42/nvim-web-devicons",
+      config = function()
+        require("trouble").setup {}
+      end
+    }
+
+    use 'nvim-lua/popup.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = { 'popup.nvim', 'plenary.nvim', 'trouble.nvim', 'telescope-fzf-native.nvim' },
+      config = function()
+        local trouble_provider_telescope = require("trouble.providers.telescope")
+
+        local telescope = require('telescope');
+        telescope.setup {
+          defaults = {
+            mappings = {
+              i = { ["<c-t>"] = trouble_provider_telescope.open_with_trouble },
+              n = { ["<c-t>"] = trouble_provider_telescope.open_with_trouble },
+            },
+          },
+        }
+        telescope.load_extension('fzf')
+
+      end
+    }
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', requires = 'telescope.nvim' }
+
+    use {
+      'nvim-treesitter/nvim-treesitter',
+
+      config = function()
+        require'nvim-treesitter.configs'.setup {
+          highlight = {
+            enable = true,
+
+            -- currently `treesitter-markdown` doesn't support all syntax
+            -- highlighting that we want (e.g. `**foo**` doesn't color that bolded
+            -- text); this allows the older regexp based highlighting to work still
+            additional_vim_regex_highlighting = { 'markdown' },
+          },
+
+          indent = {
+            enable = true,
+          },
+        }
+      end,
+
+      run = function() require('nvim-treesitter.install').update({ with_sync = true })() end,
+    }
+  end,
+  config = {
+    snapshot = snapshot_path,
+  }
+})
+
+function M.update(opts)
   opts = opts or { quit_on_install = false }
-
-  check_or_install_packer()
-
-  local packer = require 'packer'
-  local util = require 'packer.util'
-
-  local snapshot_path = util.join_paths(vim.fn.stdpath('config'), 'plugins-dev.json')
 
   -- autocmd User PackerComplete quitall
   vim.api.nvim_create_autocmd('User', {
     once = true,
     pattern = 'PackerComplete',
     callback = function()
+      -- delete the existing snapshot so that we can write the new one without prompting
+      vim.fn.system('rm ' .. snapshot_path)
       packer.snapshot(snapshot_path)
 
       if opts.quit_on_install then
@@ -40,95 +123,28 @@ local function M.update(opts)
     end
   })
 
-  packer.startup({
-    function(use)
-      use 'wbthomason/packer.nvim'
-      use 'neovim/nvim-lspconfig'
-      use 'tpope/vim-sensible'
-      use 'editorconfig/editorconfig-vim'
-      use 'tpope/vim-fugitive'
-      use 'tpope/vim-rhubarb' -- make fugitive understand github.com &co
-      use 'tpope/vim-git'
-      use 'tpope/vim-surround'
-      use 'christoomey/vim-tmux-navigator'
-      use 'airblade/vim-gitgutter'
-      use 'wincent/terminus'
-      use 'joshdick/onedark.vim'
-      use 'kyazdani42/nvim-web-devicons'
-
-      use {
-        'kyazdani42/nvim-tree.lua',
-        config = function()
-          require'nvim-tree'.setup { }
-        end
-      }
-
-      use {
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-          require("trouble").setup {}
-        end
-      }
-
-      use 'nvim-lua/popup.nvim'
-      use 'nvim-lua/plenary.nvim'
-      use {
-        'nvim-telescope/telescope.nvim',
-        requires = { 'popup.nvim', 'plenary.nvim', 'trouble.nvim', 'telescope-fzf-native.nvim' },
-        config = function()
-          local trouble_provider_telescope = require("trouble.providers.telescope")
-
-          local telescope = require('telescope');
-          telescope.setup {
-            defaults = {
-              mappings = {
-                i = { ["<c-t>"] = trouble_provider_telescope.open_with_trouble },
-                n = { ["<c-t>"] = trouble_provider_telescope.open_with_trouble },
-              },
-            },
-          }
-          telescope.load_extension('fzf')
-
-        end
-      }
-      use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', requires = 'telescope.nvim' }
-
-      use {
-        'nvim-treesitter/nvim-treesitter',
-
-        config = function()
-          require'nvim-treesitter.configs'.setup {
-            ensure_installed = 'all',
-
-            highlight = {
-              enable = true,
-
-              -- currently `treesitter-markdown` doesn't support all syntax
-              -- highlighting that we want (e.g. `**foo**` doesn't color that bolded
-              -- text); this allows the older regexp based highlighting to work still
-              additional_vim_regex_highlighting = { 'markdown' },
-            },
-
-            indent = {
-              enable = true,
-            },
-          }
-        end,
-
-        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-      }
-    end,
-    config = {
-      snapshot = snapshot_path,
-    }
-  })
-
   packer.sync() -- Perform `PackerUpdate` and then `PackerCompile`
 end
 
-local function M.bootstrap()
-  M.update { quit_on_install = true, update_plugins = false }
+function M.bootstrap(opts)
+  opts = opts or { quit_on_install = true }
+
+  -- autocmd User PackerComplete quitall
+  vim.api.nvim_create_autocmd('User', {
+    once = true,
+    pattern = 'PackerComplete',
+    callback = function()
+      -- once installed, compile the plugins/packer_compiled.lua file
+      packer.compile();
+
+      if opts.quit_on_install then
+        vim.cmd('quitall')
+      end
+    end
+  })
+
+  -- install from plugins-dev.json lockfile
+  packer.install()
 end
 
 return M
