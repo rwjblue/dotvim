1d9e623604cfa7aa26a8c6128b9671558d098a4f
Robert Jackson <me@rwjblue.com>
2022-08-19 22:03:37 -0400

    Use auto_install with treesitter in case installation fails

--- lua/rwjblue/plugins.lua
+++ lua/rwjblue/plugins.lua
@@ -87,6 +87,8 @@ packer.startup({
 
       config = function()
         require'nvim-treesitter.configs'.setup {
+          auto_install = true,
+
           highlight = {
             enable = true,
 
