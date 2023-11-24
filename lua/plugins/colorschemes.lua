41ec481c6cef837d8703f4770b734c064fd947ea
Robert Jackson <me@rwjblue.com>
2023-11-24 10:07:19 -0500

    Add colorscheme file
    - install gruvbox and tokyonight
    - configure tokyonight as default

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,19 @@
+return {
+  -- add tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+  },
+
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
