ca82251c850a2644ce29f24da9f92537f7a96649
Robert Jackson <rjackson@linkedin.com>
2023-12-11 17:35:37 -0500

    Automatically commit whenever `lazy.nvim` finishes installing / updating

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -38,3 +38,24 @@ vim.api.nvim_create_autocmd("TermOpen", {
     vim.api.nvim_win_set_width(win_id, 100)
   end,
 })
+
+local function updatePluginLockFile()
+  -- Check if lazy-lock.json has changes
+  local hasChanges = os.execute("git diff --exit-code --quiet lazy-lock.json")
+
+  -- If there are changes (hasChanges is false), then add and commit
+  if hasChanges ~= 0 then
+    -- Get current date and time
+    local dateTime = os.date("%Y-%m-%d %H:%M:%S")
+
+    -- Run Git commands to add and commit changes
+    os.execute("git add lazy-lock.json")
+    os.execute('git commit -m "Update plugins ' .. dateTime .. '"')
+  end
+end
+
+-- Create autocommands for LazyInstall and LazyUpdate events
+vim.api.nvim_create_autocmd("User", {
+  pattern = { "LazyInstall", "LazyUpdate" },
+  callback = updatePluginLockFile,
+})
