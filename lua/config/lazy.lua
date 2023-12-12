6f45bc1e6719ddb305677d36433a3a91429fbf8f
Robert Jackson <rjackson@linkedin.com>
2023-12-12 17:46:40 -0500

    Enable extras.editor.symbols-outline

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.editor.symbols-outline" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
