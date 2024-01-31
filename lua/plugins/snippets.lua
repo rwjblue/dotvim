1d93575d5ed9bca047dccf21e42e5330fdca89d4
Robert Jackson <rjackson@linkedin.com>
2024-01-31 16:06:59 -0500

    Setup quick snippet creation/edit system
    Co-authored-by: David J. Hamilton <dhamilton@linkedin.com>

---
+++ lua/plugins/snippets.lua
@@ -0,0 +1,22 @@
+return {
+  -- disable (suggestion from @hjdivad)
+  { "rafamadriz/friendly-snippets", enabled = false },
+
+  {
+    "L3MON4D3/LuaSnip",
+    ft = "snippets",
+    config = function(_, opts)
+      require("luasnip").setup(opts)
+
+      local snippet_paths = { "~/.config/nvim/snippets" }
+      require("luasnip.loaders.from_snipmate").lazy_load({
+        paths = snippet_paths,
+      })
+      require("luasnip.loaders.from_lua").lazy_load({ paths = snippet_paths })
+
+      vim.api.nvim_create_user_command("HiEditSnippets", function()
+        require("luasnip.loaders").edit_snippet_files({})
+      end, { desc = "Edit snippets used in this buffer" })
+    end,
+  },
+}
