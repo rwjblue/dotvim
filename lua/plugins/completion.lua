400a6c90b394cecc22236a695063d13c9cf4e4bd
Robert Jackson <me@rwjblue.com>
2024-02-03 17:43:48 -0500

    Add more completion sources
    - Add emoji completion with `:` prefix 🎉
    - Add nvim_lsp_signature_help completion support
    - Link defaults for easier review

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,17 @@
+return {
+  -- the following is merged with the default nvim-cmp config from:
+  --   https://github.com/LazyVim/LazyVim/blob/v10.9.1/lua/lazyvim/plugins/coding.lua#L33-L105
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+    },
+    opts = function(_, opts)
+      vim.list_extend(opts.sources, {
+        { name = "emoji" },
+        { name = "nvim_lsp_signature_help" },
+      })
+    end,
+  },
+}
