08565085c9bed8fca33bab222bd3f5f88a6edef2
Robert Jackson <me@rwjblue.com>
2024-02-03 11:26:22 -0500

    Move treesitter-textobjects into general purpose treesitter file

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        -- add any additional parsers here
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          lsp_interop = {
+            enable = true,
+            border = "none",
+            floating_preview_opts = {},
+            peek_definition_code = {
+              ["<leader>cp"] = "@function.outer",
+              ["<leader>cP"] = "@class.outer",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
