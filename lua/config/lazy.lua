423eb28555d08f22491bd2fe2fd8fbe20714ac6b
Robert Jackson <rjackson@linkedin.com>
2024-03-11 10:57:11 -0400

    Use lazyvim.plugins.extras.lazyrc (remove homerolled version)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,13 +10,13 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim",                    import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim",                       import = "lazyvim.plugins" },
     -- import any extras modules here
     -- import/override with your plugins
     { import = "plugins" },
 
     -- add project-level extension support for `.lazy.lua` files
-    { import = "plugins.extras.util.lazyrc" },
+    { import = "lazyvim.plugins.extras.lazyrc" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
