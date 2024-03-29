98421d7f671773bc1c5a6e21778886515254cd28
Robert Jackson <rjackson@linkedin.com>
2024-03-29 13:51:31 -0400

    Tell neo-tree to not overwrite terminal windows

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    optional = true,
+    opts = {
+      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "edgy" },
+    },
+  }
+}
