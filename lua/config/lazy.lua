601576a706c25aeda83a4c364c12e825aa154157
Robert Jackson <rjackson@linkedin.com>
2024-06-11 15:59:51 -0400

    Move copilot to manually specified extras
    paves the way to conditionally use copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,8 +11,10 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim",                               import = "lazyvim.plugins" },
     -- import any extras modules here
+
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
