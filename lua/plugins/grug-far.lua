b23d7cd9666584fbcf79f2fab852e6396e82c271
Robert Jackson <rjackson@linkedin.com>
2024-08-08 13:39:51 -0400

    Use grug-far as default "live grep"

---
+++ lua/plugins/grug-far.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "MagicDuck/grug-far.nvim",
+    keys = {
+      { "<leader>/", require("grug-far").grug_far, "Grep (Root Dir)" },
+    }
+  }
+}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<leader>/", false },
+    },
+  },
+}
