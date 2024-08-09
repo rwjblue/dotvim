81fee35600224a093e525ac9a10767065122d99c
Robert Jackson <rjackson@linkedin.com>
2024-08-09 13:38:12 -0400

    Configure copilot-chat to be in a floating window (like terminal)

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    optional = true,
+    opts = {
+      -- configure the window to float with a reasonable size (basically behave like the floating terminal)
+      window = {
+        layout = "float",
+        border = "rounded",
+        width = 0.6,
+        height = 0.6,
+      }
+    }
+  }
+}
