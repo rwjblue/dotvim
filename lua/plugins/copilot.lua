45c4fc6061c5f1abf17ca509e2f40fce6e14e54c
Robert Jackson <rjackson@linkedin.com>
2024-08-09 13:38:34 -0400

    Configure copilot-chat to use gpt-4o
    Revert this when LazyVim updates the default in the extra

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,6 +3,9 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     optional = true,
     opts = {
+      -- TODO: remove this when this is the default, it will likely be updated in LazyVim's extra soon
+      model = "gpt-4o",
+
       -- configure the window to float with a reasonable size (basically behave like the floating terminal)
       window = {
         layout = "float",
