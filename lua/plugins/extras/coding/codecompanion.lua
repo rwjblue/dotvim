6eca56e526ff31e649678e24109df9aa38622ebc
Robert Jackson <me@rwjblue.com>
2024-09-29 17:44:13 -0400

    Handle changes in codecompanion.nvim v4

--- lua/plugins/extras/coding/codecompanion.lua
+++ lua/plugins/extras/coding/codecompanion.lua
@@ -1,8 +1,8 @@
--- from https://github.com/LazyVim/LazyVim/pull/4268
+-- originally from https://github.com/LazyVim/LazyVim/pull/4268
 return {
   {
     "olimorris/codecompanion.nvim",
-    cmd = { "CodeCompanion", "CodeCompanionActions", "CodeCompanionToggle", "CodeCompanionAdd", "CodeCompanionChat" },
+    cmd = { "CodeCompanion", "CodeCompanionActions", "CodeCompanionChat" },
 
     opts = function()
       local options = require("codecompanion.config")
@@ -26,11 +26,11 @@ return {
     end,
 
     keys = {
-      { "<leader>a",  "",                              desc = "+ai",        mode = { "n", "v" } },
-      { "<leader>aA", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Prompt Actions (CodeCompanion)" },
-      { "<leader>aa", "<cmd>CodeCompanionToggle<cr>",  mode = { "n", "v" }, desc = "Toggle (CodeCompanion)" },
-      { "<leader>ac", "<cmd>CodeCompanionAdd<cr>",     mode = "v",          desc = "Add code to CodeCompanion" },
-      { "<leader>ap", "<cmd>CodeCompanion<cr>",        mode = "n",          desc = "Inline prompt (CodeCompanion)" },
+      { "<leader>a",  "",                                  desc = "+ai",        mode = { "n", "v" } },
+      { "<leader>aA", "<cmd>CodeCompanionActions<cr>",     mode = { "n", "v" }, desc = "Prompt Actions (CodeCompanion)" },
+      { "<leader>aa", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "Toggle (CodeCompanion)" },
+      { "<leader>ac", "<cmd>CodeCompanionChat Add<cr>",    mode = "v",          desc = "Add code to CodeCompanion" },
+      { "<leader>ap", "<cmd>CodeCompanion<cr>",            mode = "n",          desc = "Inline prompt (CodeCompanion)" },
     },
   },
 
