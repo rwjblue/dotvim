a55ecd739323d06a7527628ef350d3891324c7fd
Robert Jackson <rjackson@linkedin.com>
2024-10-23 11:44:04 -0400

    chore(config): enable exrc option without version check**
    - Remove version check for Neovim 0.9.0 when enabling `exrc`.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,9 +4,7 @@
 
 vim.opt.relativenumber = false
 
-if vim.fn.has("nvim-0.9.0") == 1 then
-  -- automatically run trusted .nvim.lua .nvimrc .exrc
-  -- :trust <file> to trust
-  -- :trust ++deny/++remove <file>
-  vim.opt.exrc = true
-end
+-- automatically run trusted .nvim.lua .nvimrc .exrc
+-- :trust <file> to trust
+-- :trust ++deny/++remove <file>
+vim.opt.exrc = true
