c99790832745d24eca58cdbde795b5a7c943b947
Robert Jackson <rjackson@linkedin.com>
2024-11-04 11:44:08 -0500

    Migrate to using blink.cmp (via LazyVim Extra)

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,17 +1,26 @@
 return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      -- experimental signature help support
+      trigger = {
+        signature_help = { enabled = true }
+      }
+    }
+  }
   -- the following is merged with the default nvim-cmp config from:
   --   https://github.com/LazyVim/LazyVim/blob/v10.9.1/lua/lazyvim/plugins/coding.lua#L33-L105
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-    },
-    opts = function(_, opts)
-      vim.list_extend(opts.sources, {
-        { name = "emoji" },
-        { name = "nvim_lsp_signature_help" },
-      })
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = {
+  --     "hrsh7th/cmp-emoji",
+  --     "hrsh7th/cmp-nvim-lsp-signature-help",
+  --   },
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.sources, {
+  --       { name = "emoji" },
+  --       { name = "nvim_lsp_signature_help" },
+  --     })
+  --   end,
+  -- },
 }
