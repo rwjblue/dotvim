847679789f338ba92072db1c2b282db2bea9b8d1
Robert Jackson <me@rwjblue.com>
2024-11-11 07:09:34 -0800

    Use claude as default Avante provider

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -6,9 +6,17 @@ return {
     "yetone/avante.nvim",
     optional = true,
     opts = {
-      provider = "openai",
+      provider = "claude",
       openai = {
-        api_key_name = { "op", "item", "get", "OpenAI - nvim Token", "--vault", "Rob (Work)", "--fields", "label=credential", "--reveal" },
+        -- api_key_name = { "op", "item", "get", "OpenAI - nvim Token", "--vault", "Rob (Work)", "--fields", "label=credential", "--reveal" },
+      },
+      claude = {
+        -- NOTE: this would be better, but it's a bit annoying at the moment
+        -- since Avante.nvim is eagerly ran the 1password prompt happens on
+        -- every launch of neovim
+        --
+        -- api_key_name = { "op", "item", "get", "Claude - nvim Token", "--vault", "Rob (Work)", "--fields", "label=credential", "--reveal" },
+        api_key_name = "AI_CLAUDE_API_KEY",
       }
     }
   }
