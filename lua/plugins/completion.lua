68d614578c22808569614cd5eda8cbd36beda7a0
Robert Jackson <me@rwjblue.com>
2024-11-16 21:15:24 -0500

    Make it easier to test out blink.cmp
    This enables/disables the various cmp setups that I'm testing out a bit
    more easily.

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,26 +1,30 @@
+local use_blink = false;
+
 return {
   {
     "saghen/blink.cmp",
+    enabled = use_blink,
     opts = {
       -- experimental signature help support
       trigger = {
         signature_help = { enabled = true }
       }
     }
-  }
+  },
   -- the following is merged with the default nvim-cmp config from:
   --   https://github.com/LazyVim/LazyVim/blob/v10.9.1/lua/lazyvim/plugins/coding.lua#L33-L105
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   dependencies = {
-  --     "hrsh7th/cmp-emoji",
-  --     "hrsh7th/cmp-nvim-lsp-signature-help",
-  --   },
-  --   opts = function(_, opts)
-  --     vim.list_extend(opts.sources, {
-  --       { name = "emoji" },
-  --       { name = "nvim_lsp_signature_help" },
-  --     })
-  --   end,
-  -- },
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = not use_blink,
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+    },
+    opts = function(_, opts)
+      vim.list_extend(opts.sources, {
+        { name = "emoji" },
+        { name = "nvim_lsp_signature_help" },
+      })
+    end,
+  },
 }
