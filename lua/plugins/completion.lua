c167124fca8b24d9f64655cc954be3bd2b9f969a
Robert Jackson <me@rwjblue.com>
2025-01-06 20:57:00 -0500

    Add blink-emoji.nvim to completion sources

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -7,4 +7,31 @@ return {
       -- signature = { enabled = true }
     }
   },
+
+  -- blink-emoji
+  {
+    "moyiz/blink-emoji.nvim",
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      -- TODO: this enables experimental signature help support, but seems like
+      -- that duplicates signature help (at least with rustacean)
+      -- signature = { enabled = true }
+      --
+      sources = {
+        default = {
+          "emoji",
+        },
+        providers = {
+          emoji = {
+            module = "blink-emoji",
+            name = "Emoji",
+            score_offset = 15,        -- Tune by preference
+            opts = { insert = true }, -- Insert emoji (default) or complete its name
+          }
+        }
+      }
+    }
+  },
 }
