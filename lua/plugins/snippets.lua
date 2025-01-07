206512cb4f9032f4577002b3dd6b49b093aea5ac
Robert Jackson <me@rwjblue.com>
2025-01-06 21:15:54 -0500

    Integrate LuaSnip into blink.cmp configuration

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -69,6 +69,7 @@ return {
     -- automatically inherits the default configuration from:
     --   https://github.com/LazyVim/LazyVim/blob/v10.9.1/lua/lazyvim/plugins/coding.lua#L3-L31
     "L3MON4D3/LuaSnip",
+    version = "v2.*",
 
     -- ensure that the plugin is loaded if we open any `.snippets` files (enables syntax hihglighting)
     event = { "BufRead *.snippets" },
@@ -80,4 +81,29 @@ return {
       require("luasnip.loaders.from_lua").lazy_load()
     end,
   },
+
+  {
+    'saghen/blink.cmp',
+    version = '*',
+    -- !Important! Make sure you're using the latest release of LuaSnip
+    -- `main` does not work at the moment (2025-01-06)
+    dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+
+    opts = {
+      snippets = {
+        expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+
+        active = function(filter)
+          if filter and filter.direction then
+            return require('luasnip').jumpable(filter.direction)
+          end
+          return require('luasnip').in_snippet()
+        end,
+        jump = function(direction) require('luasnip').jump(direction) end,
+      },
+      sources = {
+        default = { 'luasnip' },
+      },
+    }
+  }
 }
