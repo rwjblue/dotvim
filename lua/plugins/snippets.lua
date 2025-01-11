f53bd43cd8c0570525c3368712597abdec32d5f6
Robert Jackson <me@rwjblue.com>
2025-01-10 21:13:04 -0500

    Fix blink.cmp config for luasnip
    In 0.10.0 they added a dedicated preset system (supporting luasnip and
    mini.snippets). So we can remove some annoying code and rely on
    blink.cmp to do it for us!! 🎉

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -90,19 +90,9 @@ return {
     dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
 
     opts = {
-      snippets = {
-        expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-
-        active = function(filter)
-          if filter and filter.direction then
-            return require('luasnip').jumpable(filter.direction)
-          end
-          return require('luasnip').in_snippet()
-        end,
-        jump = function(direction) require('luasnip').jump(direction) end,
-      },
+      snippets = { preset = 'luasnip' },
       sources = {
-        default = { 'luasnip' },
+        default = { 'snippets' },
       },
     }
   }
