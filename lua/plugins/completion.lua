be8604778af6a30b5ef8c0fd81518cd64774a1ce
Robert Jackson <me@rwjblue.com>
2025-01-23 17:05:43 -0500

    refactor(completion): Tweak priority of various completion sources
    - Make buffer & emoji to have a lower priority than other sources
    - Make snippets higher priority

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -5,20 +5,30 @@ return {
       -- TODO: this enables experimental signature help support, but seems like
       -- that duplicates signature help (at least with rustacean)
       -- signature = { enabled = true }
+      sources = {
+        providers = {
+          snippets = {
+            -- snippets should be a higher priority by default
+            score_offset = 10,
+          },
+
+          buffer = {
+            -- deprioritize buffer completions, mainly to always get them below
+            -- snippet, lsp, &c
+            score_offset = -100,
+          },
+        }
+      }
     }
   },
 
-  -- blink-emoji
+  -- blink-emoji setup
   {
     "moyiz/blink-emoji.nvim",
   },
   {
     "saghen/blink.cmp",
     opts = {
-      -- TODO: this enables experimental signature help support, but seems like
-      -- that duplicates signature help (at least with rustacean)
-      -- signature = { enabled = true }
-      --
       sources = {
         default = {
           "emoji",
@@ -27,7 +37,10 @@ return {
           emoji = {
             module = "blink-emoji",
             name = "Emoji",
-            score_offset = 15,        -- Tune by preference
+            -- set emoji to have a slightly lower priority than other sources
+            -- (completion in rust/typescript codebases should get lsp sourced
+            -- results higher than emoji)
+            score_offset = -10,
             opts = { insert = true }, -- Insert emoji (default) or complete its name
           }
         }
