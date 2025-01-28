0a37d4bdbb896a1a280630602203e2243e998f12
Robert Jackson <me@rwjblue.com>
2025-01-28 15:39:23 -0500

    fix(snippets): Ensure nvim specific gitcommit snippet only works in nvim config

--- snippets/gitcommit.lua
+++ snippets/gitcommit.lua
@@ -7,12 +7,23 @@ local f = ls.function_node
 local d = ls.dynamic_node
 local sn = ls.snippet_node
 
+local function is_neovim_config_repo()
+  local cwd = vim.fn.getcwd()
+  local target_path = vim.fn.expand("$HOME/src/rwjblue/dotvim")
+
+  return cwd == target_path
+end
+
 return {
   s(
-    { trig = "update-plugins" },
+    {
+      trig = "update-plugins",
+      show_condition = is_neovim_config_repo,
+    },
     {
       t("chore(plugins): Update lockfile "),
       f(function() return os.date("%Y-%m-%d %H:%M:%S") end)
-    }
+    },
+    { condition = is_neovim_config_repo }
   )
 }
