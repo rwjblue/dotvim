30156992fddd99285035d6431c4516a2b460fef5
Robert Jackson <me@rwjblue.com>
2025-02-01 08:10:39 -0500

    chore: Move auto commit code into rwjblue module
    This still needs work to account for jujutsu, but I wanted to
    consolidate where most of my custom code lives.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -46,29 +46,3 @@ vim.api.nvim_create_autocmd("TermOpen", {
   end,
 })
 
-local function updatePluginLockFile()
-  -- Check if lazy-lock.json has changes
-  local hasChanges = os.execute("git diff --exit-code --quiet lazy-lock.json")
-
-  -- If there are changes (hasChanges is false), then add and commit
-  if hasChanges ~= 0 then
-    -- Get current date and time
-    local dateTime = os.date("%Y-%m-%d %H:%M:%S")
-
-    -- Run Git commands to add and commit changes
-    os.execute("git add lazy-lock.json > /dev/null 2>&1")
-    os.execute('git commit --quiet -m "Update plugins ' .. dateTime .. '"')
-  end
-end
-
--- Create autocommands for LazyInstall and LazyUpdate events
--- TODO: bring this back if/when we have a better idea of what to do in Jujutsu
--- vim.api.nvim_create_autocmd("User", {
---   pattern = "LazyInstall",
---   callback = updatePluginLockFile,
--- })
---
--- vim.api.nvim_create_autocmd("User", {
---   pattern = "LazyUpdate",
---   callback = updatePluginLockFile,
--- })
--- lua/rwjblue/init.lua
+++ lua/rwjblue/init.lua
@@ -12,4 +12,34 @@ function M.get_diff()
   end
 end
 
+function M.commit_lockfile()
+  -- TODO: this needs to be updated to work for jujutsu
+  -- Check if lazy-lock.json has changes
+  local hasChanges = os.execute("git diff --exit-code --quiet lazy-lock.json")
+
+  -- If there are changes (hasChanges is false), then add and commit
+  if hasChanges ~= 0 then
+    -- Get current date and time
+    local dateTime = os.date("%Y-%m-%d %H:%M:%S")
+
+    -- Run Git commands to add and commit changes
+    os.execute("git add lazy-lock.json > /dev/null 2>&1")
+    os.execute('git commit --quiet -m "Update plugins ' .. dateTime .. '"')
+  end
+end
+
+function M.setup_auto_commit_for_lockfile()
+  -- Create autocommands for LazyInstall and LazyUpdate events
+  -- TODO: bring this back if/when we have a better idea of what to do in Jujutsu
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "LazyInstall",
+    callback = M.commit_lockfile,
+  })
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "LazyUpdate",
+    callback = M.commit_lockfile,
+  })
+end
+
 return M
