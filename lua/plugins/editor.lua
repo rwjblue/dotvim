be105f1e2cbcc8a144861dfe9c787a6a97cf5a0d
Robert Jackson <me@rwjblue.com>
2025-02-10 15:59:58 -0500

    feat!: Try out `snacks.explorer` and `snacks.picker`

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,12 +1,41 @@
 return {
   -- file explorer
   {
+    -- copied and modified from
+    -- https://github.com/LazyVim/LazyVim/blob/v14.11.0/lua/lazyvim/plugins/extras/editor/snacks_explorer.lua
+    --
+    -- Swap `<leader>e` and `<leader>E` to make `e` do cwd instead of root dir
+    "folke/snacks.nvim",
+    optional = true,
+    opts = { explorer = {} },
+    keys = {
+      {
+        "<leader>fE",
+        function()
+          Snacks.explorer({ cwd = LazyVim.root() })
+        end,
+        desc = "Explorer Snacks (root dir)",
+      },
+      {
+        "<leader>fe",
+        function()
+          Snacks.explorer()
+        end,
+        desc = "Explorer Snacks (cwd)",
+      },
+      { "<leader>e", "<leader>fe", desc = "Explorer Snacks (cwd)",      remap = true },
+      { "<leader>E", "<leader>fE", desc = "Explorer Snacks (root dir)", remap = true },
+    },
+  },
+
+  {
+    -- copied and modified from
+    -- https://github.com/LazyVim/LazyVim/blob/v14.8.0/lua/lazyvim/plugins/editor.lua#L22-L23
+    -- make `<leader>e` always default to CWD
     "nvim-neo-tree/neo-tree.nvim",
+    optional = true,
     cmd = "Neotree",
     keys = {
-      -- copied and modified from
-      -- https://github.com/LazyVim/LazyVim/blob/v14.8.0/lua/lazyvim/plugins/editor.lua#L22-L23
-      -- make `<leader>e` always default to CWD
       { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)",      remap = true },
       { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
     }
--- lua/plugins/neo-tree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    optional = true,
-    opts = {
-      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "edgy" },
-    },
-  }
-}
