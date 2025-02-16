6e79ff632722b2e94aaebb78e2d4fe27b2fd115c
Robert Jackson <me@rwjblue.com>
2025-02-16 11:33:42 -0500

    fix(conform.nvim): Fix issues with conform.nvim

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,25 +1,22 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  opts = {
-    -- Define your formatters
-    formatters_by_ft = {
-      lua = { "stylua" },
-      -- disable python formatter, which will force conform.nvim to fallback to ruff instead
-      python = {},
-      json = { "fixjson" },
-      javascript = { { "prettierd", "prettier" } },
-    },
-    -- Customize formatters
-    formatters = {
-      shfmt = {
-        prepend_args = { "-i", "2" },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- disable python formatter, which will force conform.nvim to fallback to ruff instead
+        python = {},
+        json = { "fixjson" },
+        javascript = { { "prettierd", "prettier" } },
+      },
+      -- Customize formatters
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
       },
     },
-  },
-  init = function()
-    -- If you want the formatexpr, here is the place to set it
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
+  }
 }
